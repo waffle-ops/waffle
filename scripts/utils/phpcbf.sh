d843cf21cd76bf41c9b2b00a1f8a3f6584046bc0
Curtis Ogle <curtis.jams.ogle@gmail.com>
2020-10-28 20:12:20 -0400

    #1: Create helper scripts for PHPCS and PHPCBF.

---
+++ scripts/utils/phpcbf.sh
@@ -0,0 +1,9 @@
+#!/bin/bash 
+
+PHPCBF=./vendor/bin/phpcbf
+
+if [ ! -f "$PHPCS" ]; then
+    echo "Unable to finde PHPCBF at $PHPCBF. Make sure that you are in the root of the Waffles directory and that dev dependencies have been installed via composer."
+fi
+
+$PHPCBF --standard=PSR1,PSR2,PSR12 src
