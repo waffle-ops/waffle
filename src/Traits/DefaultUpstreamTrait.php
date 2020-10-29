2618c4c16efca9b7d62ad1c813685546bbbc8420
Curtis Ogle <curtis.jams.ogle@gmail.com>
2020-10-28 23:41:47 -0400

    #3: Add a trait to determine the default upstream for sync commands.

---
+++ src/Traits/DefaultUpstreamTrait.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Waffles\Traits;
+
+trait DefaultUpstreamTrait
+{
+    
+    /**
+     * getDefaultUpstream
+     *
+     * Gets the default upstream option.
+     *
+     * @return string
+     */
+    private function getDefaultUpstream()
+    {
+        $config = $this->getConfig();
+
+        if (isset($config['default_upstream'])) {
+            return $config['default_upstream'];
+        }
+
+        // If we know this is a Pantheon site, let's use live.
+        if ($config['host'] === 'pantheon') {
+            return 'live';
+        }
+
+        // Acquia-like sites are most common, so if all else fails, go with 'prod'.
+        return 'prod';
+    }
+}
