1ef10e27a83cf4eda270454426f191e857c17992
Curtis Ogle <curtis.jams.ogle@gmail.com>
2020-10-28 22:50:25 -0400

    #3: Create a base command for all other commands to extend.

---
+++ src/Command/BaseCommand.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Waffles\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Waffles\Model\Config\ProjectConfig;
+
+class BaseCommand extends Command
+{
+    
+    /**
+     * getConfig
+     *
+     * Gets the project configuration from the ProjectConfig singleton.
+     *
+     * @return array
+     */
+    protected function getConfig()
+    {
+        $project_config = ProjectConfig::getInstance();
+        return $project_config->getProjectConfig();
+    }
+}
