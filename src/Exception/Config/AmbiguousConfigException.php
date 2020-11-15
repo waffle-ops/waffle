08edacb449fabfbf41a81015f523161a4148e6f1
Curtis Ogle <curtis.jams.ogle@gmail.com>
2020-11-15 16:40:33 -0500

    Correct PHPCS issues.

--- src/Exception/Config/AmbiguousConfigException.php
+++ src/Exception/Config/AmbiguousConfigException.php
@@ -10,7 +10,7 @@ class AmbiguousConfigException extends Exception
     private const MESSAGE = <<<MSG
         Error: Multiple .waffle.yml files have been detected.
         The .waffle.yml file should be located in the docroot of the project or in repository root of the project.
-    MSG;
+MSG;
 
     public function __construct()
     {
