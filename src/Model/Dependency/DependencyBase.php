ad62796f860e5d512d48baed1fd74f954d819018
Curtis Ogle <curtis.jams.ogle@gmail.com>
2020-12-11 00:05:33 -0500

    Remove unused dependency code.

--- src/Model/Dependency/DependencyBase.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace Waffle\Model\Dependency;
-
-abstract class DependencyBase implements DependencyInterface
-{
-
-}
--- src/Model/Dependency/DependencyManager.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace Waffle\Model\Dependency;
-
-class DependencyManager
-{
-
-}
