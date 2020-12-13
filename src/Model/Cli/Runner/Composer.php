43ce9910942dd0c4cfd5551c270828d1f9063d7a
Curtis Ogle <curtis.jams.ogle@gmail.com>
2020-12-13 16:16:11 -0500

    Create foundation for Composer commands.

---
+++ src/Model/Cli/Runner/Composer.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Waffle\Model\Cli\Runner;
+
+class Composer
+{
+
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+
+    }
+
+    // TODO: Add methods here as needed. Should mimic the Drush runner class, generally.
+}
