4bdc74f58e9424a953b82d45a6d129b629366eef
Curtis Ogle <curtis.jams.ogle@gmail.com>
2020-12-17 22:33:49 -0500

    Update bootstrap file to load, compile, and use the DI container.

--- bin/waffle.php
+++ bin/waffle.php
@@ -2,10 +2,18 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Waffle\Application;
-use Waffle\Model\IO\IO;
 
-$io = IO::getInstance();
+// Load and compile the DI container.
+$container = new ContainerBuilder();
+$loader = new YamlFileLoader($container, new FileLocator());
+$loader->load(__DIR__ . '/../config/services.yml');
+$container->compile();
 
-$app = new Application();
-$app->run($io->getInput(), $io->getOutput());
+// Loading the application from the container to take advantage of the ability
+// to inject the commands in the DI layer.
+$application = $container->get(Application::class);
+$application->run();
