f943adf8245135f04dd76a5b292c3542d5590441
Curtis Ogle <curtis.jams.ogle@gmail.com>
2021-01-31 18:01:07 -0500

    Add helper method for highlighting output text.

--- src/Model/IO/IOStyle.php
+++ src/Model/IO/IOStyle.php
@@ -19,4 +19,24 @@ class IOStyle extends SymfonyStyle implements StyleInterface
     {
         $this->block($message, null, 'fg=yellow', ' ! ');
     }
+
+    /**
+     * Helper method to highlight text.
+     *
+     * @param $text
+     *   The string that contains text that should be highlighted as %s. Text
+     *   will be green in color.
+     * @param $replacements
+     *   The replacement text to be highlighted. Text will be yellow in color.
+     */
+    public function highlightText($text, $replacements) {
+        $message = '<info>' . $text . '</info>';
+        $tokens = [];
+
+        foreach ($replacements as $replacement) {
+            $tokens[] = '<comment>' . $replacement . '</comment>';
+        }
+
+        $this->writeln(vsprintf($message, $tokens));
+    }
 }
