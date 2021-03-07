1424116cb156bd0942affce260e425ad33ba5d60
Curtis Ogle <curtis.jams.ogle@gmail.com>
2021-03-07 16:56:55 -0500

    Implement remaining configuration keys from previous configuration system.

---
+++ src/Model/Config/Item/CommandPrefix.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Waffle\Model\Config\Item;
+
+use Symfony\Component\Config\Definition\Builder\NodeBuilder;
+use Waffle\Model\Config\BaseConfigItem;
+use Waffle\Model\Config\ConfigItemInterface;
+
+class CommandPrefix extends BaseConfigItem
+{
+    /**
+     * @todo Consider removing command_prefix as a config option. For now, this
+     * is included for backwards compatabilty.
+     *
+     * This currently serves to blindly add a prefix to CLI commands. Some
+     * projects may use lando and may need to prefix 'lando' to some commands.
+     * Chances are we will need to accept a xxx_prefix command key for things
+     * like npm, drush, composer, etc...
+     *
+     * Leaving this for now until we figure out a better way of calling those
+     * commands. I think w factory pattern will work well. The factory could
+     * build the command and attach the prefix and add anything else that
+     * may be needed.
+     */
+
+    /**
+     * @var string
+     *
+     * The key for this config item.
+     */
+    public const KEY = 'command_prefix';
+
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        parent::__construct(
+            self::KEY,
+            [
+                ConfigItemInterface::SCOPE_PROJECT,
+                ConfigItemInterface::SCOPE_LOCAL,
+            ]
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getDefinition()
+    {
+        $nodeBuilder = new NodeBuilder();
+        return $nodeBuilder->scalarNode(self::KEY);
+    }
+}
--- src/Model/Context/Context.php
+++ src/Model/Context/Context.php
@@ -7,10 +7,14 @@ use Symfony\Component\Config\Definition\Processor;
 use Waffle\Model\Config\ConfigTreeService;
 use Waffle\Model\Config\Item\Alias;
 use Waffle\Model\Config\Item\Cms;
+use Waffle\Model\Config\Item\CommandPrefix;
+use Waffle\Model\Config\Item\ComposerPath;
 use Waffle\Model\Config\Item\DefaultUpstream;
 use Waffle\Model\Config\Item\Host;
+use Waffle\Model\Config\Item\LocalSettingsFilename;
 use Waffle\Model\Config\Item\Recipes;
 use Waffle\Model\Config\Item\Tasks;
+use Waffle\Model\Config\Item\Timeout;
 use Waffle\Model\Config\Item\Upstreams;
 
 class Context implements ConfigurationInterface
@@ -199,5 +203,102 @@ class Context implements ConfigurationInterface
     public function getTasks()
     {
         return $this->get(Tasks::KEY);
+    }
+
+    /**
+     * Gets the upstreams value as defined in the config file.
+     *
+     * @todo See src/Model/Config/Item/Upstreams.php
+     *
+     * @return string[]
+     */
+    public function getUpstreams()
+    {
+        /**
+         * @todo We should convert this to an array node so that the Symfony
+         * config processor just hands us an array.
+         */
+
+        $raw_upstreams =  $this->get(Upstreams::KEY) ?? '';
+        $allowed_upstreams = explode(',', $raw_upstreams);
+        return $allowed_upstreams;
+    }
+
+    /**
+     * Gets the composer path as defined in the config file.
+     *
+     * @todo See src/Model/Config/Item/ComposerPath.php
+     *
+     * @return string
+     */
+    public function getComposerPath()
+    {
+        /**
+         * @todo The code below is part of the previous implementation of this,
+         * that needs to be re-implemented.
+         */
+
+        // if (!$this->keyExists(self::KEY_COMPOSER_PATH)) {
+        //     // Attempt to derive the composer.json path.
+        //     $this->set(self::KEY_COMPOSER_PATH, $this->determineComposerPath());
+        // }
+
+        return $this->get(ComposerPath::KEY);
+    }
+
+    /**
+     * Gets the command prefix.
+     *
+     * @todo See src/Model/Config/Item/CommandPrefix.php
+     *
+     * @return array|string|null
+     */
+    public function getCommandPrefix()
+    {
+        return $this->get(CommandPrefix::KEY);
+    }
+
+    /**
+     * Gets the local settings filename.
+     *
+     * @todo See src/Model/Config/Item/LocalSettingFilename.php
+     *
+     * Default value: settings.local.php
+     *
+     * @return array|string|null
+     */
+    public function getLocalSettingsFilename()
+    {
+        /**
+         * @todo The code below is part of the previous implementation of this,
+         * that needs to be re-implemented.
+         */
+
+        // if (!$this->keyExists(self::KEY_LOCAL_SETTINGS_FILENAME)) {
+        //     $this->set(self::KEY_LOCAL_SETTINGS_FILENAME, 'settings.local.php');
+        // }
+
+        return $this->get(LocalSettingsFilename::KEY);
+    }
+
+    /**
+     * Gets the timeout time (in seconds) for commands.
+     *
+     * @todo See src/Model/Config/Item/Timeout.php
+     *
+     * @return array|string|null
+     */
+    public function getTimeout()
+    {
+        /**
+         * @todo The code below is part of the previous implementation of this,
+         * that needs to be re-implemented.
+         */
+
+        // if (!$this->keyExists(self::KEY_TIMEOUT)) {
+        //     $this->set(self::KEY_TIMEOUT, 300);
+        // }
+
+        return $this->get(Timeout::KEY);
     }
 }
