c7db5a4d0d900dba61c103673000c33a3b176c06
Curtis Ogle <curtis.jams.ogle@gmail.com>
2021-03-24 09:25:37 -0400

    Remove old comments from the login task.

--- src/Command/Task/Login.php
+++ src/Command/Task/Login.php
@@ -55,8 +55,6 @@ class Login extends BaseCommand implements DiscoverableTaskInterface
             $process = $sync->postSyncLogin();
             $url = $process->getOutput();
             $this->io->success(sprintf('User Login: %s', $url));
-            // TODO: Attempt to open the url with the browser. Drush has
-            // already solved this problem. Check to see how they solved it.
         } catch (\Exception $e) {
             $this->io->error($e->getMessage());
             return Command::FAILURE;
