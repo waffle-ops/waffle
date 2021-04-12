ea77b3dbd9424217c6d8bbe8403dbce03008d3ff
Curtis Ogle <curtis.jams.ogle@gmail.com>
2021-04-12 00:34:02 -0400

    Update the self-update command to return an integer.

--- src/Command/Command/SelfUpdate.php
+++ src/Command/Command/SelfUpdate.php
@@ -3,6 +3,9 @@
 namespace Waffle\Command\Command;
 
 use SelfUpdate\SelfUpdateCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
 use Waffle\Application as Waffle;
 use Waffle\Command\DiscoverableCommandInterface;
 use Waffle\Helper\PharHelper;
@@ -26,6 +29,20 @@ class SelfUpdate extends SelfUpdateCommand implements DiscoverableCommandInterfa
     {
         parent::configure();
         $this->setName(self::COMMAND_KEY);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        try {
+            parent::execute($input, $output);
+        } catch (\Exception $e) {
+            // Intentionally blank.
+        }
+
+        return Command::SUCCESS;
     }
 
     /**
