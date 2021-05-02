748b556cd9fd0b7e0bf32d5e22094b3e1b3c0be5
Curtis Ogle <curtis.jams.ogle@gmail.com>
2021-05-02 19:02:49 -0400

    Remove audit report generation from the AuditService.

--- src/Model/Audit/AuditService.php
+++ src/Model/Audit/AuditService.php
@@ -3,7 +3,6 @@
 namespace Waffle\Model\Audit;
 
 use Waffle\Helper\DiHelper;
-use Waffle\Model\Audit\AuditManager;
 
 class AuditService
 {
@@ -29,40 +28,12 @@ class AuditService
      *
      * The checks are loaded from the container this way so that we do not load
      * the audit checks unless explicitly asked for via the audit command.
+     *
+     * @return AuditCheckInterface[]
      */
-    private function getAuditChecks()
+    public function getAuditChecks()
     {
         $auditManager = $this->diHelper->getContainer()->get(AuditManager::class);
         return $auditManager->getAuditChecks();
-    }
-
-    /**
-     * Validates all applicable audit checks and returns a report.
-     *
-     * @return array
-     */
-    public function getAuditReport()
-    {
-        $auditChecks = $this->getAuditChecks();
-
-        $report = [];
-
-        foreach ($auditChecks as $auditCheck) {
-            if (!$auditCheck->isApplicable()) {
-                continue;
-            }
-
-            $success = $auditCheck->validate();
-
-            if ($auditCheck->validate()) {
-                $key = $auditCheck->getDescription();
-            } else {
-                $key = $auditCheck->getResolution();
-            }
-
-            $report[$key] = $success;
-        }
-
-        return $report;
     }
 }
