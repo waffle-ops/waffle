eacd8fc44edba669d04f545a2407959b3c8b136e
Curtis Ogle <curtis.jams.ogle@gmail.com>
2021-05-23 17:00:05 -0400

    #82 - Update gulp base command class to use the configured gulp binary.

--- src/Model/Cli/Command/GulpCommand.php
+++ src/Model/Cli/Command/GulpCommand.php
@@ -3,6 +3,7 @@
 namespace Waffle\Model\Cli\Command;
 
 use Waffle\Model\Cli\BaseCliCommand;
+use Waffle\Model\Config\Item\Bin;
 use Waffle\Model\Context\Context;
 
 class GulpCommand extends BaseCliCommand
@@ -13,7 +14,8 @@ class GulpCommand extends BaseCliCommand
      */
     public function __construct(Context $context, array $args)
     {
-        array_unshift($args, 'gulp');
+        $binary = $context->getBin(Bin::BIN_GULP);
+        array_unshift($args, $binary);
         parent::__construct($context, $args);
     }
 }
