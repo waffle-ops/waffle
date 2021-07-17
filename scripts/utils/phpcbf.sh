f438e0b0ea637f060331252a9adb34d661fa67f4
Curtis Ogle <curtis.jams.ogle@gmail.com>
2021-07-17 12:32:03 -0400

    Move test and util scripts into the .waffle.yml file.

--- scripts/utils/phpcbf.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash 
-
-PHPCBF=./vendor/bin/phpcbf
-
-if [ ! -f "$PHPCS" ]; then
-    echo "Unable to finde PHPCBF at $PHPCBF. Make sure that you are in the root of the Waffle directory and that dev dependencies have been installed via composer."
-fi
-
-$PHPCBF --standard=PSR1,PSR2,PSR12 src
