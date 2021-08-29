0920411abb861282fa98afa621b58f9454772b4f
Curtis Ogle <curtis.jams.ogle@gmail.com>
2021-08-28 22:10:17 -0400

    Simplify the service locator implementation to be more intuitive.

--- src/Model/Build/BackendBuildHandlerFactory.php
+++ src/Model/Build/BackendBuildHandlerFactory.php
@@ -2,14 +2,40 @@
 
 namespace Waffle\Model\Build;
 
+use Psr\Container\ContainerInterface;
 use Symfony\Contracts\Service\ServiceSubscriberInterface;
-use Symfony\Contracts\Service\ServiceSubscriberTrait;
 use Waffle\Model\Build\Backend\ComposerBackendBuildHandler;
 use Waffle\Model\Config\Item\BuildBackend;
 
 class BackendBuildHandlerFactory implements ServiceSubscriberInterface
 {
-    use ServiceSubscriberTrait;
+    /**
+     * Service locator.
+     *
+     * @var ContainerInterface
+     */
+    private $locator;
+
+    /**
+     * Constructor for BackendBuildHandlerFactory
+     *
+     * @param ContainerInterface $locator
+     */
+    public function __construct(ContainerInterface $locator)
+    {
+        $this->locator = $locator;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public static function getSubscribedServices(): array
+    {
+        return [
+            BuildBackend::STRATEGY_NONE => NullBuildHandler::class,
+            BuildBackend::STRATEGY_COMPOSER => ComposerBackendBuildHandler::class,
+        ];
+    }
 
     /**
      * Gets a instance of a backend builder.
@@ -20,44 +46,13 @@ class BackendBuildHandlerFactory implements ServiceSubscriberInterface
      */
     public function getHandler(string $strategy)
     {
-        switch ($strategy) {
-            case BuildBackend::STRATEGY_NONE:
-                return $this->none();
-
-            case BuildBackend::STRATEGY_COMPOSER:
-                return $this->composer();
-
-            default:
-                throw new \Exception(sprintf(
-                    'Backend build strategy \'%s\' not implemented.',
-                    $strategy
-                ));
+        if ($this->locator->has($strategy)) {
+            return $this->locator->get($strategy);
         }
-    }
 
-    /**
-     * Gets the 'none' backend build handler.
-     *
-     * The method name is intentional.
-     * See https://symfony.com/doc/current/service_container/service_subscribers_locators.html#service-subscriber-trait
-     *
-     * @return NullBuildHandler
-     */
-    private function none(): NullBuildHandler
-    {
-        return $this->container->get(__METHOD__);
-    }
-
-    /**
-     * Gets the 'composer' backend build handler.
-     *
-     * The method name is intentional.
-     * See https://symfony.com/doc/current/service_container/service_subscribers_locators.html#service-subscriber-trait
-     *
-     * @return ComposerBackendBuildHandler
-     */
-    private function composer(): ComposerBackendBuildHandler
-    {
-        return $this->container->get(__METHOD__);
+        throw new \Exception(sprintf(
+            'Backend build strategy \'%s\' not implemented.',
+            $strategy
+        ));
     }
 }
